7d55504b73fdd7c36c330281c230d2662df7a8f6
RaptorRush <raptorrush135@hotmail.com>
2024-11-03 19:45:54 -0600

    Move role seeding to OnModelCreating

--- src/GymApp/Database/AppDbContext.cs
+++ src/GymApp/Database/AppDbContext.cs
@@ -1,10 +1,32 @@
 ﻿namespace GymApp.Database;
 
 using GymApp.Database.Entities;
+using GymApp.Identity;
 
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore;
 
+using RaptorUtils.AspNet.Identity;
+
 public class AppDbContext(
     DbContextOptions<AppDbContext> options)
-    : IdentityDbContext<AppUser>(options);
+    : IdentityDbContext<AppUser>(options)
+{
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+
+        SeedRoles(builder);
+    }
+
+    private static void SeedRoles(ModelBuilder builder)
+    {
+        var appNamespace = new Guid("f3cf2703-30de-4ca2-a10f-1f2f63a2972c");
+        var roleGenerator = new IdentityRoleGenerator(appNamespace);
+
+        IEnumerable<IdentityRole> roles = roleGenerator.Generate<UserRole>();
+
+        builder.Entity<IdentityRole>().HasData(roles);
+    }
+}
