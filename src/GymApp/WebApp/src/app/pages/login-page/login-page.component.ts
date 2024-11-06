eb1edd2c7783081194d85f930bc784b6a6bbdcb5
RafaMadera18 <rafa_madlop@hotmail.com>
2024-11-05 21:29:54 -0600

    Add Main Page

--- src/GymApp/WebApp/src/app/pages/login-page/login-page.component.ts
+++ src/GymApp/WebApp/src/app/pages/login-page/login-page.component.ts
@@ -1,12 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Router } from "@angular/router";
 
 @Component({
-  selector: 'app-login-page',
+  selector: "app-login-page",
   standalone: true,
   imports: [],
-  templateUrl: './login-page.component.html',
-  styleUrl: './login-page.component.scss'
+  templateUrl: "./login-page.component.html",
+  styleUrl: "./login-page.component.scss",
 })
 export class LoginPageComponent {
+  constructor(private readonly _router: Router) {}
 
+  public onLogin(): void {
+    this._router.navigate(["/main-page"]);
+  }
 }
