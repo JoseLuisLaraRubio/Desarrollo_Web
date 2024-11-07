13fd345cd014d1f36e7af19ddea3cc107e08a868
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2024-11-07 14:31:13 -0600

    update paegs with the new navbar component

--- src/GymApp/WebApp/src/app/pages/home-page/home-page.component.ts
+++ src/GymApp/WebApp/src/app/pages/home-page/home-page.component.ts
@@ -1,11 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
-import { HomeNavBarComponent } from '../../components/home-nav-bar/home-nav-bar.component';
+import { NavBarComponent } from "../../components/nav-bar/nav-bar.component";
 
 @Component({
   selector: 'app-home-page',
   standalone: true,
-  imports: [RouterLink, HomeNavBarComponent],
+  imports: [RouterLink, NavBarComponent],
   templateUrl: './home-page.component.html',
   styleUrl: './home-page.component.scss'
 })
