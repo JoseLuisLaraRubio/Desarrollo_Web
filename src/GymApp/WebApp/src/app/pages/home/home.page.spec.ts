c938503018e1e615f35bf92a7706e8265d32dfc0
RafaMadera18 <rafa_madlop@hotmail.com>
2024-11-28 11:48:39 -0600

    Add Home Page And Base Project

--- src/GymApp/WebApp/src/app/pages/home/home.page.spec.ts
+++ src/GymApp/WebApp/src/app/pages/home/home.page.spec.ts
@@ -1,18 +1,17 @@
-import { ComponentFixture, TestBed } from "@angular/core/testing";
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { HomePage } from './home.page';
 
-import { HomePage } from "./home.page";
-
-describe("HomePage", () => {
+describe('HomePage', () => {
   let component: HomePage;
   let fixture: ComponentFixture<HomePage>;
 
-  beforeEach(async () => {
+  beforeEach(() => {
     fixture = TestBed.createComponent(HomePage);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
 
-  it("should create", () => {
+  it('should create', () => {
     expect(component).toBeTruthy();
   });
 });
