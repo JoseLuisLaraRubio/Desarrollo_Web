7bc1014533abb5cb397cd398e514038c2452a26b
RaptorRush <raptorrush135@hotmail.com>
2024-11-30 23:53:29 -0600

    RoutineProgressDataMapper: Refactor property ignore

--- src/GymApp/ApiService/Features/Progress/Data/RoutineProgressDataMapper.cs
+++ src/GymApp/ApiService/Features/Progress/Data/RoutineProgressDataMapper.cs
@@ -7,20 +7,18 @@ using Riok.Mapperly.Abstractions;
 [Mapper]
 public static partial class RoutineProgressDataMapper
 {
-    [MapValue(nameof(RoutineProgress.Id), Use = nameof(EmptyId))]
-    [MapValue(nameof(RoutineProgress.Routine), Use = nameof(IgnoreRoutine))]
+    [MapperIgnoreTarget(nameof(RoutineProgress.Id))]
+    [MapValue(nameof(RoutineProgress.Routine), Use = nameof(NullRoutine))]
     public static partial RoutineProgress DataToEntity(RoutineProgressDataWithId data);
 
-    [MapValue(nameof(RoutineBlockResult.Id), Use = nameof(EmptyId))]
-    [MapValue(nameof(RoutineBlockResult.Exercise), Use = nameof(IgnoreExercise))]
+    [MapperIgnoreTarget(nameof(RoutineBlockResult.Id))]
+    [MapValue(nameof(RoutineBlockResult.Exercise), Use = nameof(NullExercise))]
     private static partial RoutineBlockResult DataToEntity(RoutineBlockResultData data);
 
-    [MapValue(nameof(SetResult.Id), 0)]
+    [MapperIgnoreTarget(nameof(SetResult.Id))]
     private static partial SetResult DataToEntity(SetResultData data);
 
-    private static Guid EmptyId() => Guid.Empty;
+    private static Routine NullRoutine() => null!;
 
-    private static Routine IgnoreRoutine() => null!;
-
-    private static Exercise IgnoreExercise() => null!;
+    private static Exercise NullExercise() => null!;
 }
