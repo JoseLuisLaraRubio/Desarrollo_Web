02246e937f05a262bc37f3b73bf281f6f4acfc34
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2024-12-02 08:57:01 -0600

    create reusable user navbar component

---
+++ src/GymApp/WebApp/src/app/components/user-nav-bar/nav-bar.component.ts
@@ -0,0 +1,17 @@
+import { Component, Input } from '@angular/core';
+import { RouterLink } from '@angular/router';
+
+@Component({
+  selector: 'app-nav-bar',
+  standalone: true,
+  imports: [RouterLink],
+  templateUrl: './nav-bar.component.html',
+  styleUrl: './nav-bar.component.scss'
+})
+export class NavBarComponent {
+  @Input() logo = true;
+  @Input() profile = false;
+  @Input() routine = false;
+  @Input() login = false;
+  @Input() register = false;
+}
