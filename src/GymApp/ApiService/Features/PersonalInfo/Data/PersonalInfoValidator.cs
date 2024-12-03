a00014ad281d534068ddb79723b874a891739940
RaptorRush <raptorrush135@hotmail.com>
2024-12-03 03:22:17 -0600

    Add PersonalInfo Validator

---
+++ src/GymApp/ApiService/Features/PersonalInfo/Data/PersonalInfoValidator.cs
@@ -0,0 +1,17 @@
+﻿namespace GymApp.ApiService.Features.PersonalInfo.Data;
+
+using FluentValidation;
+
+using GymApp.Database.Entities;
+
+public sealed class PersonalInfoValidator : AbstractValidator<PersonalInfo>
+{
+    public PersonalInfoValidator()
+    {
+        this.RuleFor(p => p.FullName).Length(1, 50);
+
+        this.RuleFor(p => p.Height).GreaterThan(0);
+
+        this.RuleFor(p => p.Weight).GreaterThan(0);
+    }
+}
