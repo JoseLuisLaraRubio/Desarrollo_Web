166073fb64c1423395b6e6e42cfef83026154994
RafaMadera18 <rafa_madlop@hotmail.com>
2024-12-03 10:27:17 -0600

    Fix App Nav Bar - Add Exit Button

--- src/GymApp/WebApp/src/app/components/user-nav-bar/user-nav-bar.component.ts
+++ src/GymApp/WebApp/src/app/components/user-nav-bar/user-nav-bar.component.ts
@@ -1,17 +1,27 @@
-import { Component, Input } from '@angular/core';
-import { RouterLink } from '@angular/router';
+import { Component, Input } from "@angular/core";
+import { Router, RouterLink } from "@angular/router";
+import { AuthService } from "@services/auth";
 
 @Component({
-  selector: 'app-user-nav-bar',
+  selector: "app-user-nav-bar",
   standalone: true,
   imports: [RouterLink],
-  templateUrl: './user-nav-bar.component.html',
-  styleUrl: './user-nav-bar.component.scss'
+  templateUrl: "./user-nav-bar.component.html",
+  styleUrl: "./user-nav-bar.component.scss",
 })
 export class UserNavBarComponent {
-  @Input() logo = true;
+  constructor(
+    private readonly _authService: AuthService,
+    private readonly _router: Router,
+  ) {}
+
   @Input() profile = false;
   @Input() routine = false;
   @Input() login = false;
   @Input() register = false;
+
+  public onClickToExit(): void {
+    this._authService.logout();
+    this._router.navigate(["/auth/home"])
+  }
 }
