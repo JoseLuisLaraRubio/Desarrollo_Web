094f7f2332f682e9a7a7d01a16e82234c82b3bd9
Isaías Rodríguez <a22216371@alumnos.uady.mx>
2024-12-03 09:47:18 -0600

    Help rafa

--- src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
+++ src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { Navbar2Component } from "@components/navbar2/navbar2.component";
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-user-profile',
@@ -10,20 +12,65 @@ import { Navbar2Component } from "@components/navbar2/navbar2.component";
   standalone: true,
   imports: [CommonModule, FormsModule, Navbar2Component]
 })
-export class UserProfilePage {
+export class UserProfilePage implements OnInit {
   user = {
-    username: 'gymlover123',
     fullName: '',
-    age: null,
-    height: null,
     sex: '',
+    dateOfBirth: '',
+    height: null,
     weight: null,
     bodyType: '',
   };
 
+  constructor(private http: HttpClient) { }
+
+  ngOnInit(): void {
+    this.getPersonalInfo();
+  }
+
+  // Obtener la información personal del usuario
+  getPersonalInfo() {
+    const url = `api/personal-info`;
+    this.http.get<any>(url)
+      .subscribe(
+        (response) => {
+          this.user = { ...this.user, ...response };
+        },
+        (error) => {
+          console.error('Error al obtener la información personal:', error);
+        }
+      );
+  }
+
   onSubmit() {
-    // Aquí podrías enviar la información actualizada al backend
-    console.log('Información del usuario:', this.user);
-    alert('¡Tu perfil ha sido actualizado!');
+    if (!this.user.fullName || !this.user.height || !this.user.weight || !this.user.sex || !this.user.bodyType || !this.user.dateOfBirth) {
+      alert('Por favor, llena todos los campos antes de guardar.');
+      return;
+    }
+
+    const url = `api/personal-info`;
+    this.http.put(url, this.user)
+      .subscribe(
+        (response) => {
+          alert('¡Tu perfil ha sido actualizado!');
+          this.getPersonalInfo();
+        },
+        (error) => {
+          console.error('Error al actualizar la información:', error);
+          alert('Error al guardar los cambios.');
+        }
+      );
+  }
+
+
+  onCancel() {
+    this.user = {
+      fullName: '',
+      sex: '',
+      dateOfBirth: '',
+      height: null,
+      weight: null,
+      bodyType: '',
+    };
   }
 }
