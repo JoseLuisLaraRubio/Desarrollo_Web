5046d968578c02a137c098598a661707e5e04082
RafaMadera18 <rafa_madlop@hotmail.com>
2024-12-03 13:35:10 -0600

    Palma Prueba esto

---
+++ src/GymApp/WebApp/src/app/services/personal-info/personal-info.service.ts
@@ -0,0 +1,27 @@
+import { HttpClient } from "@angular/common/http";
+import { Injectable } from "@angular/core";
+import { Observable } from "rxjs";
+import { PersonalInfo } from "./data";
+
+@Injectable({
+  providedIn: "root",
+})
+export class PersonalInfoService {
+  constructor(private readonly _httpClient: HttpClient) {}
+
+  public getPersonalInfo(): Observable<PersonalInfo> {
+    const apiPath = this.getApiPath();
+
+    return this._httpClient.get<PersonalInfo>(apiPath);
+  }
+
+  public updatePersonalInfo(personalInfo: PersonalInfo): Observable<void> {
+    const apiPath = this.getApiPath();
+
+    return this._httpClient.put<void>(apiPath, personalInfo);
+  }
+
+  private getApiPath(path: string = ""): string {
+    return `api/personal-info/${path}`;
+  }
+}
