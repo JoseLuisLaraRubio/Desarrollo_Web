93cf05933948d0a2480db8e0c96bc4089eaeac19
RafaMadera18 <rafa_madlop@hotmail.com>
2024-12-02 21:01:51 -0600

    Add User Info Menu

--- src/GymApp/WebApp/src/app/pages/menu/menu.page.ts
+++ src/GymApp/WebApp/src/app/pages/menu/menu.page.ts
@@ -1,13 +1,25 @@
-import { Component, OnInit } from '@angular/core';
+import { CommonModule } from "@angular/common";
+import { Component } from "@angular/core";
 import { NavBarComponent } from "@components/nav-bar/nav-bar.component";
-
+import { Nullable } from "@customTypes/nullable";
+import { AuthService, UserInfoResponse } from "@services/auth";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-menu',
-  templateUrl: './menu.page.html',
-  styleUrls: ['./menu.page.scss'],
+  selector: "app-menu",
+  templateUrl: "./menu.page.html",
+  styleUrls: ["./menu.page.scss"],
   standalone: true,
-  imports: [NavBarComponent],
+  imports: [NavBarComponent, CommonModule],
 })
 export class MenuPage {
+  private readonly _userInfo: Observable<Nullable<UserInfoResponse>>;
+
+  constructor(private readonly _authService: AuthService) {
+    this._userInfo = _authService.getUserInfo();
+  }
+
+  public get userInfo() {
+    return this._userInfo;
+  }
 }
