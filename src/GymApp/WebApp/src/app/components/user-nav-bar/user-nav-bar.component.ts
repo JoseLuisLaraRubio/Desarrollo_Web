dd68eb4ffed25f50576d31cdde275709bd200d66
RafaMadera18 <rafa_madlop@hotmail.com>
2024-12-03 15:58:25 -0600

    Fix Route User Nav Bar

--- src/GymApp/WebApp/src/app/components/user-nav-bar/user-nav-bar.component.ts
+++ src/GymApp/WebApp/src/app/components/user-nav-bar/user-nav-bar.component.ts
@@ -1,11 +1,11 @@
 import { Component, Input } from "@angular/core";
-import { Router, RouterLink } from "@angular/router";
+import { Router } from "@angular/router";
 import { AuthService } from "@services/auth";
 
 @Component({
   selector: "app-user-nav-bar",
   standalone: true,
-  imports: [RouterLink],
+  imports: [],
   templateUrl: "./user-nav-bar.component.html",
   styleUrl: "./user-nav-bar.component.scss",
 })
@@ -19,6 +19,10 @@ export class UserNavBarComponent {
   @Input() routine = false;
   @Input() login = false;
   @Input() register = false;
+
+  public onClickToMenu(): void {
+    this._router.navigate(["/menu"]);
+  }
 
   public onClickToExit(): void {
     this._authService.logout().subscribe();
