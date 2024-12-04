188d818a948e65af0452591bef67bd95f0aa9668
Isaías Rodríguez <a22216371@alumnos.uady.mx>
2024-12-03 18:47:26 -0600

    Add print user profile page

--- src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
+++ src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
@@ -14,15 +14,22 @@ import { PersonalInfo } from "@services/personal-info/data";
 })
 export class UserProfilePage implements OnInit {
   personalInfo: PersonalInfo = {} as PersonalInfo;
+  isPrinting: boolean = false; // Nuevo estado para manejar el formato de impresión
 
-  constructor(private readonly _personalInfoService: PersonalInfoService) {}
+  constructor(private readonly _personalInfoService: PersonalInfoService) { }
 
   public ngOnInit(): void {
     this.getPersonalInfo();
   }
 
   public onClickToPrint(): void {
-    window.print();
+    this.isPrinting = true; // Cambia al modo de impresión
+
+    // Llama a la función de impresión del navegador
+    setTimeout(() => {
+      window.print();
+      this.isPrinting = false; // Restaura el estado después de la impresión
+    }, 100); // Da tiempo a Angular para aplicar cambios en el DOM
   }
 
   public onSubmit(): void {
@@ -51,9 +58,7 @@ export class UserProfilePage implements OnInit {
   }
 
   private getPersonalInfo(): void {
-    const personalInfoObservable = this._personalInfoService.getPersonalInfo();
-
-    personalInfoObservable.subscribe((info) => {
+    this._personalInfoService.getPersonalInfo().subscribe((info) => {
       this.personalInfo = { ...info };
     });
   }
