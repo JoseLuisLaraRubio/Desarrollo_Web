caab4545dcc386f5de1e4b3857c3d997309a7121
RafaMadera18 <rafa_madlop@hotmail.com>
2024-12-04 20:55:17 -0600

    Refactor Validation User-profile

--- src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
+++ src/GymApp/WebApp/src/app/pages/user-profile/user-profile.page.ts
@@ -16,30 +16,32 @@ export class UserProfilePage implements OnInit {
   personalInfo: PersonalInfo = {} as PersonalInfo;
   isPrinting: boolean = false; // Nuevo estado para manejar el formato de impresión
 
-  constructor(private readonly _personalInfoService: PersonalInfoService) { }
+  constructor(private readonly _personalInfoService: PersonalInfoService) {}
 
   public ngOnInit(): void {
     this.getPersonalInfo();
   }
 
   public onClickToPrint(): void {
-    this.isPrinting = true; // Cambia al modo de impresión
-
-    // Llama a la función de impresión del navegador
+    this.isPrinting = true;
+    this.getPersonalInfo();
     setTimeout(() => {
       window.print();
-      this.isPrinting = false; // Restaura el estado después de la impresión
-    }, 100); // Da tiempo a Angular para aplicar cambios en el DOM
+      this.isPrinting = false;
+    }, 100);
   }
 
   public onSubmit(): void {
-    if (this.isPersonalInfoValid()) {
-      alert("Por favor, llena todos los campos antes de guardar.");
+    if (
+      !this.isPersonalInfoValid() &&
+      this.isHeightValid() &&
+      this.isWeightValid()
+    ) {
+      this.updatePersonalInfo(this.personalInfo);
+      this.getPersonalInfo();
       return;
     }
-
-    this.updatePersonalInfo(this.personalInfo);
-    this.getPersonalInfo();
+    alert("Por favor, llena todos los campos correctamente antes de guardar.");
   }
 
   private updatePersonalInfo(personalInfo: PersonalInfo): void {
@@ -55,6 +57,14 @@ export class UserProfilePage implements OnInit {
       !this.personalInfo.bodyType ||
       !this.personalInfo.dateOfBirth
     );
+  }
+
+  private isHeightValid(): boolean {
+    return this.personalInfo.height > 0;
+  }
+
+  private isWeightValid(): boolean {
+    return this.personalInfo.weight > 0;
   }
 
   private getPersonalInfo(): void {
