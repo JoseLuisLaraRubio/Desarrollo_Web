dd1fa36abd0581f596fc5d5899e4ede06d9e543c
JoseLuisLaraRubio <a22216373@alumnos.uady.mx>
2024-12-05 02:18:25 -0600

    seed exercise and muscle values

--- src/GymApp/ApiService/Features/Quizzes/Data/RoutineQuiz.cs
+++ src/GymApp/ApiService/Features/Quizzes/Data/RoutineQuiz.cs
@@ -5,5 +5,13 @@ public static class RoutineQuiz
     public static Quiz Instance { get; } = new([
         new("Question 1", ["Option A", "Option B", "Option C"]),
         new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
+        new("Question 2", ["Option A", "Option B", "Option C"]),
     ]);
 }
